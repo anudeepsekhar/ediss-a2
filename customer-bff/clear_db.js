7d09f5df676f0728f57160f1605282718606c029
Anudeepsekhar Bolimera <anudeep.sekhar@gmail.com>
2023-04-09 09:18:37 -0400

    fixing bugs in customer-bff

---
+++ customer-bff/clear_db.js
@@ -0,0 +1,29 @@
+import mysql from "mysql"
+
+const db = mysql.createConnection({
+    host     : "a2-databasereplicainstance-kgzn5fw5kkcj.cdzfxmkoehy9.us-east-1.rds.amazonaws.com",
+    user     : "ediss",
+    password : "password",
+    port     : 3306,
+    database :"bookstore"
+})
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database: ', err);
+      return;
+    }
+    console.log('Connected to books database!');
+  });
+  
+const q1 = "DELETE FROM books"
+db.query(q1, (err, data)=>{
+  if(err) console.log(err);
+  console.log(data);
+});
+
+const q2 = "DELETE FROM customers"
+db.query(q2, (err, data)=>{
+  if(err) console.log(err);
+  console.log(data);
+});
